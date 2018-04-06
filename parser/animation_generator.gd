d2df4e1ed894bb51eb2ca35176d88d978c460096
Christian Flach <cmfcmf.flach@gmail.com>
2018-04-07 00:17:15 +0200

    Parse animations

---
+++ parser/animation_generator.gd
@@ -0,0 +1,59 @@
+extends Node
+
+const ANIMATION_PATH = "res://imported/animations"
+
+func generate_animations(figure_data):
+	var animation_dir = Directory.new()
+	assert(animation_dir.make_dir_recursive(ANIMATION_PATH) == OK)
+
+	for item_name in figure_data['objects']['FIGUR']['items'].keys():
+		var item = figure_data['objects']['FIGUR']['items'][item_name]
+		if len(item['nested_objects']) == 0:
+			continue
+
+		assert(item['nested_objects'].has('ANIM'))
+
+		var base_gfx = item['Gfx']
+
+		for animation_idx in item['nested_objects']['ANIM'].keys():
+			var animation = item['nested_objects']['ANIM'][animation_idx]
+
+			if animation['Kind'] != 'ENDLESS':
+				# TODO support non-endless
+				continue
+			var gfx = 1 + base_gfx + animation['AnimOffs']
+			var num_steps = animation['AnimAnz']
+			var speed = 1000.0 / animation['AnimSpeed']
+			var gfx_per_step = animation['AnimAdd']
+
+			var sf = SpriteFrames.new()
+
+			sf.set_animation_loop("default", true)
+			sf.set_animation_speed("default", speed)
+
+			var gfx_filename = figure_data['gfx_category_map'][item['GfxCategory']]
+			assert(gfx_filename.begins_with('GFX'))
+			gfx_filename = gfx_filename.substr(len('GFX'), len(gfx_filename) - len('GFX'))
+
+			for step in range(num_steps):
+				sf.add_frame("default", ResourceLoader.load("res://imported/%s/%s.png" % [gfx_filename, gfx]))
+				gfx += gfx_per_step
+
+			assert(ResourceSaver.save("%s/%s_%s.res" % [ANIMATION_PATH, item_name, animation_idx], sf) == OK)
+
+
+		#	var a = Animation.new()
+		#	a.loop = true
+		#	a.length = num_steps * speed / 1000.0
+		#	a.step = speed
+		#
+		#	var track_idx = a.add_track(Animation.TYPE_VALUE)
+		#	a.track_set_enabled(track_idx, true)
+		#	a.value_track_set_update_mode(track_idx, Animation.UPDATE_DISCRETE)
+		#	a.track_set_path(track_idx, NodePath("animated_sprite:texture"))
+		#
+		#	var time = 0
+		#	for step in range(num_steps):
+		#		a.track_insert_key(track_idx, time, "res://imported/effects/%s.png" % gfx)
+		#		time += a.step
+		#		gfx += gfx_per_step
--- parser/parsers.gd
+++ parser/parsers.gd
@@ -1,22 +1,38 @@
 extends Node
 
 func parse(dir):
+	#var figure_data2 = $cod_parser.txt_to_json("res://imported/figuren.cod.txt", "res://imported/figuren.cod.json")
+	#$animation_generator.generate_animations(figure_data2)
+	#return
+	
 	copy_islands(dir)
 	
 	# Import .cod files
 	$cod_parser.cod_to_txt(dir + "/haeuser.cod", "res://imported/haeuser.cod.txt")
 	$cod_parser.cod_to_txt(dir + "/figuren.cod", "res://imported/figuren.cod.txt")
 	var object_data = $cod_parser.txt_to_json("res://imported/haeuser.cod.txt", "res://imported/haeuser.cod.json")
-	# TODO: Doesn't work yet!
-	#$cod_parser.txt_to_json("res://imported/figuren.cod.txt", "res://imported/figuren.cod.json")
+	var figure_data = $cod_parser.txt_to_json("res://imported/figuren.cod.txt", "res://imported/figuren.cod.json")
 	
 	# Import .bsh files - takes a long time.
+	$bsh_parser.bsh_convert(dir + "/GFX/NUMBERS.BSH", "res://imported/NUMBERS")
 	$bsh_parser.bsh_convert(dir + "/GFX/STADTFLD.BSH", "res://imported/stadtfld")
+	
+	$bsh_parser.bsh_convert(dir + "/GFX/EFFEKTE.BSH", "res://imported/EFFEKT")
+	$bsh_parser.bsh_convert(dir + "/GFX/FISCHE.BSH", "res://imported/WAL")
+	$bsh_parser.bsh_convert(dir + "/GFX/GAUKLER.BSH", "res://imported/GAUKLER")
+	$bsh_parser.bsh_convert(dir + "/GFX/MAEHER.BSH", "res://imported/MAEHER")
+	$bsh_parser.bsh_convert(dir + "/GFX/SCHATTEN.BSH", "res://imported/SCHATTEN")
+	$bsh_parser.bsh_convert(dir + "/GFX/SHIP.BSH", "res://imported/SHIP")
+	$bsh_parser.bsh_convert(dir + "/GFX/SOLDAT.BSH", "res://imported/SOLDAT")
+	$bsh_parser.bsh_convert(dir + "/GFX/TIERE.BSH", "res://imported/RIND")
+	$bsh_parser.bsh_convert(dir + "/GFX/TRAEGER.BSH", "res://imported/TRAEGER")
+	
 	$bsh_parser.bsh_convert(dir + "/ToolGfx/TOOLS.BSH", "res://imported/tools")
 	
 	# Generate tileset
 	$tileset_writer.write_tileset(object_data, "res://imported/stadtfld", "res://imported/stadtfld.tres")
 	
+	$animation_generator.generate_animations(figure_data)
 	$building_generator.generate_buildings(object_data)
 
 func copy_islands(dir):
