5bee0586a32be058cb7658cfa3d471e277523b8f
Christian Flach <cmfcmf.flach@gmail.com>
2018-04-07 10:42:30 +0200

    WIP: Take number of rotations into account

--- game/game.gd
+++ game/game.gd
@@ -4,7 +4,7 @@ func _ready():
 	var game = $save_game_loader.load_game("res://imported/saves/game00.gam")
 	
 	var fields = get_field_dict(get_tree().get_nodes_in_group("fields"))
-	
+	var n_rotations = 1
 	for island in game['islands']:
 		for y in range(island['height']):
 			for x in range(island['width']):
@@ -12,7 +12,7 @@ func _ready():
 				var field_id = field['building']
 				if field_id != 0xFFFF:
 					assert(fields.has(field_id))
-					fields[field_id].draw_field($map/buildings, Vector2(island['x'] + x, island['y'] + y), field['rotation'])
+					fields[field_id].draw_field($map/buildings, Vector2(island['x'] + x, island['y'] + y), field['rotation'], n_rotations, field['ani'])
 				
 		for y in range(island['height']):
 			for x in range(island['width']):
@@ -20,7 +20,7 @@ func _ready():
 				var field_id = field['building']
 				if field_id != 0xFFFF:
 					assert(fields.has(field_id))
-					fields[field_id].draw_field($map/buildings, Vector2(island['x'] + x, island['y'] + y), field['rotation'])
+					fields[field_id].draw_field($map/buildings, Vector2(island['x'] + x, island['y'] + y), field['rotation'], n_rotations, field['ani'])
 				
 func get_field_dict(scenes):
 	var dict = {}
