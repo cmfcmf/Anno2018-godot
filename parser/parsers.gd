08728c5c1ed0c4b132ecd753b3fe1c2c8572984f
Christian Flach <cmfcmf.flach@gmail.com>
2018-05-13 11:49:00 +0200

    Generate statistics for BSH files

--- parser/parsers.gd
+++ parser/parsers.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const BSH_STATS_PATH = "user://imported/bsh_stats.json"
+
 func parse(dir):
 	copy_islands(dir)
 	copy_directory(dir + "/SAVEGAME", "user://saves", "gam")
@@ -10,27 +12,59 @@ func parse(dir):
 	var object_data = $cod_parser.txt_to_json("user://imported/haeuser.cod.txt", "user://imported/haeuser.cod.json")
 	var figure_data = $cod_parser.txt_to_json("user://imported/figuren.cod.txt", "user://imported/figuren.cod.json")
 	
+	var bsh_stats = get_bsh_stats()
+	
 	# Import .bsh files - takes a long time.
-	$bsh_parser.bsh_convert(dir + "/GFX/NUMBERS.BSH", "user://imported/NUMBERS")
-	$bsh_parser.bsh_convert(dir + "/GFX/STADTFLD.BSH", "user://imported/STADTFLD")
+	parse_bsh(dir, bsh_stats, "NUMBERS",  "NUMBERS")
+	parse_bsh(dir, bsh_stats, "STADTFLD", "STADTFLD")
 	
-	$bsh_parser.bsh_convert(dir + "/GFX/EFFEKTE.BSH", "user://imported/EFFEKT")
-	$bsh_parser.bsh_convert(dir + "/GFX/FISCHE.BSH", "user://imported/WAL")
-	$bsh_parser.bsh_convert(dir + "/GFX/GAUKLER.BSH", "user://imported/GAUKLER")
-	$bsh_parser.bsh_convert(dir + "/GFX/MAEHER.BSH", "user://imported/MAEHER")
-	$bsh_parser.bsh_convert(dir + "/GFX/SCHATTEN.BSH", "user://imported/SCHATTEN")
-	$bsh_parser.bsh_convert(dir + "/GFX/SHIP.BSH", "user://imported/SHIP")
-	$bsh_parser.bsh_convert(dir + "/GFX/SOLDAT.BSH", "user://imported/SOLDAT")
-	$bsh_parser.bsh_convert(dir + "/GFX/TIERE.BSH", "user://imported/RIND")
-	$bsh_parser.bsh_convert(dir + "/GFX/TRAEGER.BSH", "user://imported/TRAEGER")
+	parse_bsh(dir, bsh_stats, "EFFEKTE",  "EFFEKT")
+	parse_bsh(dir, bsh_stats, "FISCHE",   "WAL")
+	parse_bsh(dir, bsh_stats, "GAUKLER",  "GAUKLER")
+	parse_bsh(dir, bsh_stats, "MAEHER",   "MAEHER")
+	parse_bsh(dir, bsh_stats, "SCHATTEN", "SCHATTEN")
+	parse_bsh(dir, bsh_stats, "SHIP",     "SHIP")
+	parse_bsh(dir, bsh_stats, "SOLDAT",   "SOLDAT")
+	parse_bsh(dir, bsh_stats, "TIERE",    "RIND")
+	parse_bsh(dir, bsh_stats, "TRAEGER",  "TRAEGER")
 	
-	$bsh_parser.bsh_convert(dir + "/ToolGfx/TOOLS.BSH", "user://imported/tools")
+	parse_bsh(dir, bsh_stats, "TOOLS",    "TOOLS")
+	
+	dump_bsh_stats(bsh_stats)
 	
 	# Generate tileset
 	$tileset_writer.write_tileset(object_data, "user://imported/STADTFLD", "user://imported/STADTFLD_tileset.res")
 	
 	$animation_generator.generate_animations(figure_data)
 	$building_generator.generate_buildings(object_data)
+
+func parse_bsh(dir, bsh_stats, in_name, out_name):
+	var path = 'GFX'
+	if in_name == 'TOOLS':
+		path = 'ToolGfx'
+	
+	var result = $bsh_parser.bsh_convert(dir + "/%s/%s.BSH" % [path, in_name], "user://imported/%s" % out_name)
+	if result != null:
+		bsh_stats['GFX' + out_name] = result
+
+func get_bsh_stats():
+	var bsh_json_file = File.new()
+	if not bsh_json_file.file_exists(BSH_STATS_PATH):
+		return {}
+	
+	assert(OK == bsh_json_file.open(BSH_STATS_PATH, File.READ))
+	
+	var parse_result = JSON.parse(bsh_json_file.get_as_text())
+	assert(parse_result.error == OK)
+	bsh_json_file.close()
+	
+	return parse_result.result
+
+func dump_bsh_stats(bsh_stats):
+	var bsh_json_file = File.new()
+	assert(OK == bsh_json_file.open(BSH_STATS_PATH, File.WRITE))
+	bsh_json_file.store_string(JSON.print(bsh_stats, "  "))
+	bsh_json_file.close()
 
 func copy_islands(dir):
 	copy_directory(dir + "/NOKLIMA", "user://imported/islands/noklima", "scp")
